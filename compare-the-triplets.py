0b140112f5f22792ea6762d8e7b8136f0b1ad7cb
Afroz Chakure <aaaanchakure@gmail.com>
2019-10-18 23:49:13 +0530

    Added two problem solutions

---
+++ compare-the-triplets.py
@@ -0,0 +1,31 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the compareTriplets function below.
+def compareTriplets(a, b):
+    alice = bob = 0
+    for i in range(3):   
+        if a[i] < b[i]:
+            bob += 1
+        elif a[i] > b[i]:
+            alice +=1
+    return [alice, bob]
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    a = list(map(int, input().rstrip().split()))  # To accept multiple input in a single line
+
+    b = list(map(int, input().rstrip().split()))
+
+    result = compareTriplets(a, b)
+
+    fptr.write(' '.join(map(str, result)))
+    fptr.write('\n')
+
+    fptr.close()
