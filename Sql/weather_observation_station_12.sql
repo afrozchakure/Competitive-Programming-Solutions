febe9032b55769b22627e5350072e16acdce0bd9
Afroz Chakure <aaaanchakure@gmail.com>
2019-11-09 18:26:41 +0530

    http://www.tutorialspoint.com/mysql/mysql-regexps.htm

---
+++ Sql/weather_observation_station_12.sql
@@ -0,0 +1,11 @@
+-- http://www.tutorialspoint.com/mysql/mysql-regexps.htm
+
+-- '.' is to specify a character and '*' is to mention zero or more of the preceding character
+-- [...] -->  Any character listed between the square brackets
+-- [^...] --> Any character not listed between the square brackets
+
+SELECT distinct(CITY) FROM STATION 
+WHERE CITY RLIKE '^[^aeiouAEIOU].*[^AEIOUaeiou]$';  
+
+-- SELECT distinct(CITY) FROM STATION 
+-- WHERE CITY REGEXP '^[^aeiouAEIOU].*[^AEIOUaeiou]$';  
