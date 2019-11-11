4d6350cdf9e54e952a862699708a829916087f0d
Afroz Chakure <aaaanchakure@gmail.com>
2019-11-12 00:13:00 +0530

    made minor changes

--- Sql/revising_aggregations_averages.sql
+++ Sql/revising_aggregations_averages.sql
@@ -1,3 +1,3 @@
--- Query the average population of all cities in CITY where District is California. 
+-- Query the average population of all cities in CITY where District is California
 
 SELECT AVG(POPULATION) FROM CITY WHERE DISTRICT = 'CALIFORNIA';
