e75b37a3439bd5a398f5ac2d4a7c73c4468b2962
Afroz Chakure <aaaanchakure@gmail.com>
2019-11-16 07:54:16 +0530

    Added file to starred problems

--- Starred Problems/detect_floating_point_number.py
+++ Starred Problems/detect_floating_point_number.py
@@ -1,6 +1,21 @@
-# import re
-# print(bool(re.search(r'ly', 'similarly')))
+# re
 
-import re
-print(bool(re.match(r'ly', 'similarly')))
-print(bool(re.match(r'ly', 'ly should be in the beginning')))
+# A regular expression (or RegEx) specifies a set of strings that matches it.
+# A regex is a sequence of characters that defines a search pattern, mainly for the use of string pattern matching.
+
+# The re.search() expression scans through a string looking for the first location where the regex pattern produces a match.
+# It either returns a MatchObject instance or returns None if no position in the string matches the pattern.
+# Code
+# >>> import re
+# >>> print bool(re.search(r"ly","similarly"))
+# True
+
+
+# The re.match() expression only matches at the beginning of the string.
+# It either returns a MatchObject instance or returns None if the string does not match the pattern.
+# Code
+# >>> import re
+# >>> print bool(re.match(r"ly","similarly"))
+# False
+# >>> print bool(re.match(r"ly","ly should be in the beginning"))
+# True
