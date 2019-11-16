1779760f55f9087051f37b94f57526019992b69f
afrozchakure <aaaanchakure@gmail.com>
2019-11-16 22:45:44 +0530

    minor change to the file

--- Sql/average_population_of_each_continent.sql
+++ Sql/average_population_of_each_continent.sql
@@ -1,5 +1,5 @@
 -- Given the CITY and COUNTRY tables, query the names of all the continents (COUNTRY.Continent) 
--- and their respective average city populations (CITY.Population) rounded down to the nearest integer.
+-- and their respective average city populations (CITY.Population) rounded 'down' to the nearest integer.
 -- Note: CITY.CountryCode and COUNTRY.Code are matching key columns.
 
 SELECT COUNTRY.CONTINENT, FLOOR(AVG(CITY.POPULATION)) 
