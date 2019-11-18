808d5dbcac7b410b9592aa02f0914b8f5c2622d6
Afroz Chakure <aaaanchakure@gmail.com>
2019-11-18 23:06:58 +0530

    Added binary_tree_nodes.sql file

---
+++ Problem-Solving/Solve_me_first.py
@@ -0,0 +1,28 @@
+# Complete the function solveMeFirst to compute the sum of two integers.
+# Function prototype:
+
+# int solveMeFirst(int a, int b);
+# where,
+#     a is the first integer input.
+#     b is the second integer input
+
+
+def solveMeFirst(a,b):
+	# Hint: Type return a+b below
+    return a + b
+
+if __name__ == '__main__':
+    num1 = input()
+    num2 = input()
+    res = solveMeFirst(num1,num2)
+    print res
+
+# Sample Input
+
+# a = 2
+# b = 3
+
+# Sample Output
+
+# 5
+
