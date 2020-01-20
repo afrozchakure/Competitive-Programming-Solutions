7d4a486516bf129d74cb8797461dbf6818a97492
Afroz Chakure <aaaanchakure@gmail.com>
2020-01-20 23:21:38 +0530

    update reverse_a_linked_list.cpp file

--- Problem-Solving/reverse_a_linked_list.cpp
+++ Problem-Solving/reverse_a_linked_list.cpp
@@ -69,20 +69,53 @@ void free_singly_linked_list(SinglyLinkedListNode* node) {
  *
  */
 SinglyLinkedListNode* reverse(SinglyLinkedListNode* head) {
-    SinglyLinkedListNode *curr, *prev, *next; // Creating nodes
+    SinglyLinkedListNode *curr, *prev, *next;
     prev = NULL;
     next = NULL;
     curr = head;
     while(curr != NULL)
     {
-       next = curr->next;  // Hint : See the pattern
+       next = curr->next; // Hint : See the pattern in the code (to memorize it)
        curr->next = prev;
        prev = curr;
        curr = next; 
     }
     head = prev;
     return prev;
-
 }
 
 int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int tests;
+    cin >> tests;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int tests_itr = 0; tests_itr < tests; tests_itr++) {
+        SinglyLinkedList* llist = new SinglyLinkedList();
+
+        int llist_count;
+        cin >> llist_count;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        for (int i = 0; i < llist_count; i++) {
+            int llist_item;
+            cin >> llist_item;
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            llist->insert_node(llist_item);
+        }
+
+        SinglyLinkedListNode* llist1 = reverse(llist->head);
+
+        print_singly_linked_list(llist1, " ", fout);
+        fout << "\n";
+
+        free_singly_linked_list(llist1);
+    }
+
+    fout.close();
+
+    return 0;
+}
