94ef0025c4ef29892670f68fe1f7e1e511c8d325
Afroz Chakure <aaaanchakure@gmail.com>
2020-01-21 22:46:18 +0530

    Add merge_two_sorted_linked_lists.cpp file

---
+++ Problem-Solving/merge_two_sorted_linked_lists.cpp
@@ -0,0 +1,100 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the mergeLists function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+SinglyLinkedListNode* mergeLists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+    SinglyLinkedListNode *curr1, *curr2, *result;
+    curr1 = head1;
+    curr2 = head2;
+    result = NULL;
+    if(curr1 == NULL)
+    {
+        return curr2;
+    }
+    else if(curr2 == NULL)
+    {
+        return curr1;
+    }
+    if(curr1->data<= curr2->data)
+    {
+        result = curr1;
+        result->next = mergeLists(curr1->next, curr2);
+    }
+    else
+    {
+        result = curr2;
+        result->next = mergeLists(curr1, curr2->next);
+    }
+    return result;
+    
+
+}
+
+
+int main()
