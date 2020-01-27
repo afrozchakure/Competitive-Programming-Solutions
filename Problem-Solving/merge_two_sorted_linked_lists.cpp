ef79b44acd39e5da0c97bbd05e4e0c9d466f73ac
Afroz Chakure <aaaanchakure@gmail.com>
2020-01-27 23:17:28 +0530

    Update file merge_two_sorted_linked_lists.cpp

--- Problem-Solving/merge_two_sorted_linked_lists.cpp
+++ Problem-Solving/merge_two_sorted_linked_lists.cpp
@@ -72,7 +72,6 @@ SinglyLinkedListNode* mergeLists(SinglyLinkedListNode* head1, SinglyLinkedListNo
     SinglyLinkedListNode *curr1, *curr2, *result;
     curr1 = head1;
     curr2 = head2;
-    result = NULL;
     if(curr1 == NULL)
     {
         return curr2;
@@ -81,20 +80,65 @@ SinglyLinkedListNode* mergeLists(SinglyLinkedListNode* head1, SinglyLinkedListNo
     {
         return curr1;
     }
-    if(curr1->data<= curr2->data)
+    if(curr1->data <= curr2->data)
     {
         result = curr1;
         result->next = mergeLists(curr1->next, curr2);
     }
-    else
-    {
+    else {
         result = curr2;
         result->next = mergeLists(curr1, curr2->next);
     }
     return result;
-    
-
 }
 
 
 int main()
+{
+    ofstream fout(getenv("OUTPUT_PATH"));
+
+    int tests;
+    cin >> tests;
+    cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+    for (int tests_itr = 0; tests_itr < tests; tests_itr++) {
+        SinglyLinkedList* llist1 = new SinglyLinkedList();
+
+        int llist1_count;
+        cin >> llist1_count;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        for (int i = 0; i < llist1_count; i++) {
+            int llist1_item;
+            cin >> llist1_item;
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            llist1->insert_node(llist1_item);
+        }
+      
+      	SinglyLinkedList* llist2 = new SinglyLinkedList();
+
+        int llist2_count;
+        cin >> llist2_count;
+        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+        for (int i = 0; i < llist2_count; i++) {
+            int llist2_item;
+            cin >> llist2_item;
+            cin.ignore(numeric_limits<streamsize>::max(), '\n');
+
+            llist2->insert_node(llist2_item);
+        }
+
+        SinglyLinkedListNode* llist3 = mergeLists(llist1->head, llist2->head);
+
+        print_singly_linked_list(llist3, " ", fout);
+        fout << "\n";
+
+        free_singly_linked_list(llist3);
+    }
+
+    fout.close();
+
+    return 0;
+}
