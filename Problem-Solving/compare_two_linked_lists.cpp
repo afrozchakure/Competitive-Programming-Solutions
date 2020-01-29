984453a3c59f0ed527a284719915f6223224d975
Afroz Chakure <aaaanchakure@gmail.com>
2020-01-29 21:17:41 +0530

    Add compare_two_linked_lists.cpp file

---
+++ Problem-Solving/compare_two_linked_lists.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinglyLinkedListNode {
+    public:
+        int data;
+        SinglyLinkedListNode *next;
+
+        SinglyLinkedListNode(int node_data) {
+            this->data = node_data;
+            this->next = nullptr;
+        }
+};
+
+class SinglyLinkedList {
+    public:
+        SinglyLinkedListNode *head;
+        SinglyLinkedListNode *tail;
+
+        SinglyLinkedList() {
+            this->head = nullptr;
+            this->tail = nullptr;
+        }
+
+        void insert_node(int node_data) {
+            SinglyLinkedListNode* node = new SinglyLinkedListNode(node_data);
+
+            if (!this->head) {
+                this->head = node;
+            } else {
+                this->tail->next = node;
+            }
+
+            this->tail = node;
+        }
+};
+
+void print_singly_linked_list(SinglyLinkedListNode* node, string sep, ofstream& fout) {
+    while (node) {
+        fout << node->data;
+
+        node = node->next;
+
+        if (node) {
+            fout << sep;
+        }
+    }
+}
+
+void free_singly_linked_list(SinglyLinkedListNode* node) {
+    while (node) {
+        SinglyLinkedListNode* temp = node;
+        node = node->next;
+
+        free(temp);
+    }
+}
+
+// Complete the compare_lists function below.
+
+/*
+ * For your reference:
+ *
+ * SinglyLinkedListNode {
+ *     int data;
+ *     SinglyLinkedListNode* next;
+ * };
+ *
+ */
+bool compare_lists(SinglyLinkedListNode* head1, SinglyLinkedListNode* head2) {
+    SinglyLinkedListNode *curr1, *curr2;
+    curr1 = head1;
+    curr2 = head2;
+    if(curr1 == NULL && curr2 == NULL)
+    {
+        return true;
+    }
+    else if(curr1 == NULL || curr2 == NULL)
+    {
+        return false;
+    }
+    else if(curr1->data == curr2->data)
+    {
+        return compare_lists(curr1->next, curr2->next);
+    }
+    else  // if the values in curr1 and curr2 are different (return 0)
+    {
+        return 0;
+    }
+}
+
+int main()
