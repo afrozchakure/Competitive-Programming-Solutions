7ed13be92c0106274071e771fa03bf615e1d2297
Afroz Chakure <aaaanchakure@gmail.com>
2020-02-05 09:56:59 +0530

    Added tree_height_of_a_binary_tree.cpp file

---
+++ Problem-Solving/tree_height_of_a_binary_tree.cpp
@@ -0,0 +1,85 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Node {
+    public:
+        int data;
+        Node *left;
+        Node *right;
+        Node(int d) {
+            data = d;
+            left = NULL;
+            right = NULL;
+        }
+};
+
+class Solution {
+    public:
+  		Node* insert(Node* root, int data) {
+            if(root == NULL) {
+                return new Node(data);
+            } else {
+                Node* cur;
+                if(data <= root->data) {
+                    cur = insert(root->left, data);
+                    root->left = cur;
+                } else {
+                    cur = insert(root->right, data);
+                    root->right = cur;
+               }
+
+               return root;
+           }
+        }
+/*The tree node has data, left child and right child 
+class Node {
+    int data;
+    Node* left;
+    Node* right;
+};
+
+*/
+    int height(Node* root) {
+        // Write your code here.
+        if(root == NULL)
+        {
+            return -1;  // You could use 0 if you want to inculde the parent node as well
+        }
+        else
+        {
+            int l_length = height(root->left);
+            int r_length = height(root->right);
+
+            if(l_length > r_length)
+            {
+                return l_length+1;
+            }
+            else
+                return r_length+1;
+        }
+    }
+
+}; //End of Solution
+
+int main() {
+  
+    Solution myTree;
+    Node* root = NULL;
+    
+    int t;
+    int data;
+
+    std::cin >> t;
+
+    while(t-- > 0) {
+        std::cin >> data;
+        root = myTree.insert(root, data);
+    }
+  
+    int height = myTree.height(root);
+    
+  	std::cout << height;
+
+    return 0;
+}
