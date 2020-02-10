c27dca4c5c675592454e8c70d258dc75e4fec8da
Afroz Chakure <aaaanchakure@gmail.com>
2020-02-10 22:46:32 +0530

    Added servant problem file

---
+++ CodeChef Problems/servant_FLOW008.py
@@ -0,0 +1,11 @@
+# cook your dish here
+try:
+    n = int(input())
+    for i in range(n):
+        x = int(input())
+        if(x<10):
+            print("What an obedient servant you are!")
+        else:
+            print("-1")
+except :
+    pass
