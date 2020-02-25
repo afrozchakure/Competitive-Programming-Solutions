4c1f7c09984e1690d4ac6622d3106f2b4b4de35a
Afroz Chakure <aaaanchakure@gmail.com>
2020-02-25 23:08:59 +0530

    Add exceptions.py file :

--- Python/exceptions.py
+++ Python/exceptions.py
@@ -18,7 +18,8 @@
 # >>> ValueError: invalid literal for int() with base 10: '#'
 
 # Handling Exceptions
-# The statements try and except can be used to handle selected exceptions. A try statement may have more than one except clause to specify handlers for different exceptions.
+# The statements try and except can be used to handle selected exceptions. 
+# A try statement may have more than one except clause to specify handlers for different exceptions.
 
 # #Code
 # try:
@@ -28,6 +29,24 @@
 
 # Output
 # Error Code: integer division or modulo by zero
+
+# Output Format
+# Print the value of a/b.
+# In the case of ZeroDivisionError or ValueError, print the error code.
+
+# Sample Input
+# 3
+# 1 0
+# 2 $
+# 3 1
+
+# Sample Output
+# Error Code: integer division or modulo by zero
+# Error Code: invalid literal for int() with base 10: '$'
+# 3
+
+# Note:
+# For integer division in Python 3 use //. 
 
 n = int(input())
 for i in range(n):
