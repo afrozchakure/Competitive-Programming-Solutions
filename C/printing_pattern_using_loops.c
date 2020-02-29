f2c30936cb6fad3bd3000b2bf622513fb086937d
Afroz Chakure <aaaanchakure@gmail.com>
2020-02-29 23:46:54 +0530

    Add printing_pattern_Using_loops.c file

---
+++ C/printing_pattern_using_loops.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+#include <math.h>
+#include <stdlib.h>
+#include<algorithm>
+#define min(a, b) ((a) < (b) ? (a) : (b))
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    int len = ((2*n)-1);
+    int m;
+    for (int row = 0; row< len; row++) {
+       for (int col = 0; col< len; col++) {
+           m = min(row, col);
+           m = min(m, len-row-1);
+           m = min(len-col-1, m);
+           printf("%d ", n-m);
+       }
+       printf("\n");
+    }
+    return 0;
+}
