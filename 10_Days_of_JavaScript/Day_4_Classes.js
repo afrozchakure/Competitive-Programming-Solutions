cd6051fb657cd368b477e6688e12992f4887ca5b
Afroz Chakure <aaaanchakure@gmail.com>
2020-04-17 23:48:27 +0530

    Added Day4 and Day5 files

---
+++ 10_Days_of_JavaScript/Day_4_Classes.js
@@ -0,0 +1,25 @@
+/*
+ * Implement a Polygon class with the following properties:
+ * 1. A constructor that takes an array of integer side lengths.
+ * 2. A 'perimeter' method that returns the sum of the Polygon's side lengths.
+ */
+class Polygon {
+    constructor(a) {
+        this.a = a;
+    }
+    perimeter() {
+        var sum = 0;
+        for (let i = 0; i < this.a.length; i++) {
+            sum = sum + this.a[i];
+        }
+        return sum;
+    }
+}
+
+const rectangle = new Polygon([10, 20, 10, 20]);
+const square = new Polygon([10, 10, 10, 10]);
+const pentagon = new Polygon([10, 20, 30, 40, 43]);
+
+console.log(rectangle.perimeter());
+console.log(square.perimeter());
+console.log(pentagon.perimeter());
