df157bc2f60b1961c70c016e2390e087baa63ee2
afrozchakaure <aaaanchakure@gmail.com>
2020-11-23 23:53:04 +0530

    Added Problems

---
+++ Leetcode/Algorithms/Medium/MaxIncreaseToKeepCitySkyline.cpp
@@ -0,0 +1,10 @@
+class Solution
+{
+public:
+    int maxIncreaseKeepingSkyline(vector<vector<int>> &grid)
+    {
+        int result = 0;
+
+        int result += min(max_col_val, max_row_val) - grid[i][j];
+    }
+};
---
+++ Leetcode/Database/SecondHighestSalary.sql
@@ -0,0 +1,3 @@
+# Write your MySQL query statement below
+
+SELECT MAX(SALARY) AS SecondHighestSalary FROM EMPLOYEE WHERE SALARY < (SELECT MAX(SALARY) FROM EMPLOYEE);
