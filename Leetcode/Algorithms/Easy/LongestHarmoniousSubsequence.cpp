9edc4a81719f66c7ddb3ce65cc58bbbe60eb0f9b
afrozchakaure <aaaanchakure@gmail.com>
2021-02-05 13:20:09 +0530

    Added 1 file

---
+++ Leetcode/Algorithms/Easy/LongestHarmoniousSubsequence.cpp
@@ -0,0 +1,62 @@
+// Method 1
+class Solution
+{
+public:
+    int findLHS(vector<int> &nums)
+    {
+        sort(nums.begin(), nums.end());
+
+        int left = 0;
+        int right = 1;
+        int result = 0;
+        while (right < nums.size())
+        {
+            int diff = nums[right] - nums[left];
+            if (diff == 1)
+            {
+                result = max(result, right - left + 1);
+            }
+
+            if (diff <= 1)
+            {
+                right++;
+            }
+            else
+            {
+                left++;
+            }
+        }
+        return result;
+    }
+};
+
+// Time Complexity - O(NlogN), as we are sorting
+// Space Complexity - O(1)
+
+// Method 2
+class Solution
+{
+public:
+    int findLHS(vector<int> &nums)
+    {
+        unordered_map<int, int> map;
+
+        int result = 0;
+        for (int i = 0; i < nums.size(); i++)
+        {
+            map[nums[i]]++;
+        }
+
+        for (auto i : map)
+        {
+            if (map.count(i.first - 1) > 0)
+            {
+                result = max(result, i.second + map.count(i.first - 1));
+            }
+        }
+        return result;
+    }
+};
+
+// Time Complexity - O(N)
+// Space Complexiy - O(N)
