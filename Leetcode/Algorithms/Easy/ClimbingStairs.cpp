6121a5e9f3cfba3b5ecc0654666ce0c46ee7964f
afrozchakaure <aaaanchakure@gmail.com>
2021-06-29 00:49:35 +0530

    Added Climbing Stairs problem

--- Leetcode/Algorithms/Easy/ClimbingStairs.cpp
+++ Leetcode/Algorithms/Easy/ClimbingStairs.cpp
@@ -42,3 +42,45 @@ public:
 
 // Time Complexity - O(n)
 // Space Complexity - O(n) -> Used vector
+
+// M3 - for easier understading 
+class Solution {
+public:
+    int climbStairs(int n) {
+	// v1 and v2 represent the last and 2nd last step of the staircase where the user has only one way to reach final step
+        int v1 = 1;
+        int v2 = 1;
+        if(n ==1)
+            return 1;
+        int v3 = v1 + v2;
+        v1 = v3;
+        for(int i=2; i<n; i++)
+        {
+            int temp = v1 + v2;
+            v2 = v1;
+            v1 = temp;
+        }
+        
+        return v1;      
+    }
+};
+
+// M4 
+class Solution {
+public:
+    int climbStairs(int n) {
+        int one = 1, two = 1;
+        
+        for(int i=0; i<n-1; i++)
+        {
+            int temp = one + two;
+            two = one;
+            one = temp;
+        }
+        
+        return one;
+    }
+};
+
+// Time Complexity - O(N), equal to size of array and not equal to 2**n solution in case of binary tree
+// Space Complexity - O(1)
