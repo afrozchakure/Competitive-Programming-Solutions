9c57f3d3b5d7ac747f7a6660afd7b679e086840b
afrozchakure <aaaanchakure@gmail.com>
2022-05-04 22:31:35 +0530

    Add MaxNumber of K sum Pairs solution

--- LeetCode/Algorithms/Medium/MaxNumberOfKSumPairs/MaxNumberOfKSumPairs.cpp
+++ LeetCode/Algorithms/Medium/MaxNumberOfKSumPairs/MaxNumberOfKSumPairs.cpp
@@ -1,30 +1,53 @@
-class Solution
-{
-public:
-    int maxOperations(vector<int> &nums, int k)
-    {
-        sort(nums.begin(), nums.end());
+// - Method - 1  (Use M2 for space efficient solution and M1 for time efficient solution) 
+// - Time Complexity - O(N) 
+// - Space Complexity - O(N) 
 
-        int start = 0;
-        int end = nums.size() - 1;
-        int count = 0;
-        while (start < end)
-        {
-            int val = nums[start] + nums[end];
-            if (val > k)
-                end--;
-            else if (val < k)
-                start++;
-            else if (val == k)
-            {
-                start++;
-                end--;
-                count++;
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        unordered_map<int, int> freqMap; 
+        int n = nums.size(); 
+        int ans = 0; 
+        for(int i=0; i<n; i++) {
+            if(freqMap[k - nums[i]] > 0) {
+                freqMap[k - nums[i]]--; 
+                ans++; 
+            } else {
+                freqMap[nums[i]]++; 
             }
         }
-        return count;
+        return ans; 
     }
 };
 
-// Time Complexity - O(NlogN)
-// Space Complexity - O(1)
+// ------------------------
+// -  Method - 2 
+// -  Time Complexity - O(NLogN) 
+// -  Space Complexity - O(1) 
+
+class Solution {
+public:
+    int maxOperations(vector<int>& nums, int k) {
+        int start =0; 
+        int end = nums.size() - 1; 
+        int ans = 0; 
+        
+        sort(nums.begin(), nums.end()); 
+        
+        while(start < end) {
+            int sum = nums[start] + nums[end]; 
+            
+            if(sum == k) {
+                start++; 
+                end--; 
+                ans++; 
+            } else if(sum < k) {
+                start++; 
+            } else if(sum > k) {
+                end--; 
+            }
+        }
+        
+        return ans; 
+    }
+};
