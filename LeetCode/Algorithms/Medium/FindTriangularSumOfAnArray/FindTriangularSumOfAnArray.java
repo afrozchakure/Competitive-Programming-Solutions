32450babe6a0e7106b66844e1cb27b1d761d251d
Afrin Chakure <afrinchakure12@gmail.com>
2025-10-01 16:53:47 +0530

    Solution to Triangular Sum (#105)
    * Added Regular Expression matching
    
    * Revert "Added Regular Expression matching"
    
    This reverts commit 4449207e2cefbf4249df6e1f13e5d9b6a83b85aa.
    
    * adding solution of daily probs

---
+++ LeetCode/Algorithms/Medium/FindTriangularSumOfAnArray/FindTriangularSumOfAnArray.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int triangularSum(int[] nums) {
+        int n = nums.length;
+        
+        for (int size = n; size > 1; size--) {
+            for (int i = 0; i < size - 1; i++) {
+                nums[i] = (nums[i] + nums[i + 1]) % 10;
+            }
+        }
+
+        return nums[0];
+    }
+}
